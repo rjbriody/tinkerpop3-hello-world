0cf8edd29d9137c7209ed984108c03ed736bb397
Bob Briody <rjbriody@mgmail.com>
2014-10-03 23:12:16 -0400

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ tinkerpop3-hello-world
 ======================
 
 Very simple "Hello World" Tinkerpop3 Groovy project
+
+The only setup I had to do in Intellij was adding the groovy folder as a source root.
---
+++ src/main/groovy/LetsGremlin.groovy
@@ -0,0 +1,14 @@
+import com.tinkerpop.gremlin.groovy.loaders.GremlinLoader
+import com.tinkerpop.gremlin.groovy.loaders.SugarLoader
+import com.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory
+
+public class LetsGremlin{
+
+    public static void main(String[] args) {
+        GremlinLoader.load()
+        SugarLoader.load()
+        def g = TinkerFactory.createModern()
+        // Let the gremlin begin!
+        g.V().each{println it.name}
+    }
+}
